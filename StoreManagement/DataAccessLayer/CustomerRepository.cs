0855a9ea658c2772950455504d608fa23cbec4e7
Thái Tuấn <ttuan.simple@gmail.com>
2025-08-09 10:19:16 +0700

    thêm customer repo

---
+++ StoreManagement/DataAccessLayer/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CustomerRepository
+    {
+        private readonly salesysdbEntities context;
+
+        public CustomerRepository(salesysdbEntities context)
+        {
+            this.context = context;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
