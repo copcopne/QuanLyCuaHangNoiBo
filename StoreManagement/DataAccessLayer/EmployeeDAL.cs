ffdeb0a124563f3036096297204ee1785f5fb2f6
Si Nguyen <copcopne@gmail.com>
2025-08-09 15:57:50 +0700

    update

---
+++ StoreManagement/DataAccessLayer/EmployeeDAL.cs
@@ -0,0 +1,82 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class EmployeeDAL
+    {
+        private readonly salesysdbEntities context;
+
+        public EmployeeDAL(salesysdbEntities context)
+        {
+            this.context = context;
+        }
+
+        public List<Employee> GetEmployees(String keyword)
+        {
+            if (string.IsNullOrEmpty(keyword))
+            {
+                return context.Employees.ToList();
+            }
+            else
+            {
+                return context.Employees
+                    .Where(e => e.FullName.Contains(keyword))
+                    .ToList();
+            }
+        }
+
+        public Employee GetEmployeeById(int employeeId)
+        {
+            return context.Employees.FirstOrDefault(e => e.EmployeeID == employeeId);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentNullException("Đối tượng nhân viên là bắt buộc!");
+            }
+            context.Employees.Add(employee);
+            context.SaveChanges();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentNullException("Đối tượng nhân viên là bắt buộc!");
+            }
+            var existingEmployee = context.Employees.FirstOrDefault(e => e.EmployeeID == employee.EmployeeID);
+            if (existingEmployee != null)
+            {
+                existingEmployee.FullName = employee.FullName;
+                existingEmployee.Position = employee.Position;
+                existingEmployee.Phone = employee.Phone;
+                existingEmployee.Address = employee.Address;
+                existingEmployee.Email = employee.Email;
+                existingEmployee.Status = employee.Status;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteEmployee(int employeeId)
+        {
+            var employee = context.Employees.FirstOrDefault(e => e.EmployeeID == employeeId);
+            if (employee != null)
+            {
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException("Nhân viên không tồn tại.");
+            }
+        }
+
+    }
+}
---
+++ StoreManagement/DataAccessLayer/UserAccountDAL.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class UserAccountDAL
+    {
+        private readonly salesysdbEntities context;
+        private readonly EmployeeDAL employeeDAL;
+
+        public UserAccountDAL(salesysdbEntities context)
+        {
+            this.context = context;
+            employeeDAL = new EmployeeDAL(context);
+        }
+
+        public List<UserAccount> GetUserAccounts(String keyword)
+        {
+            if (string.IsNullOrEmpty(keyword))
+            {
+                return context.UserAccounts.ToList();
+            }
+            else
+            {
+                return context.UserAccounts
+                    .Where(e => e.Username.Contains(keyword))
+                    .ToList();
+            }
+        }
+
+        public UserAccount GetUserAccountByUsername(String username)
+        {
+            return context.UserAccounts.FirstOrDefault(u => u.Username == username);
+        }
+
+        public void AddUserAccount(UserAccount account, Employee employee)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException("Đối tượng nhân viên và người dùng là bắt buộc!");
+            }
+
+            employeeDAL.AddEmployee(employee);
+
+
+            context.UserAccounts.Add(account);
+            account.EmployeeID = employee.EmployeeID; // set ID của nhân viên cho tài khoản
+
+            context.SaveChanges();
+        }
+
+        public void UpdateUserAccount(UserAccount account)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException("Đối tượng nhân viên là bắt buộc!");
+            }
+            var existingUser = context.UserAccounts.FirstOrDefault(u => u.Username == account.Username);
+            if (existingUser != null)
+            {
+
+                existingUser.PasswordHash = account.PasswordHash;
+                existingUser.Role = account.Role;
+                existingUser.IsActive = account.IsActive;
+
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteUserAccount(String username)
+        {
+            var user = context.UserAccounts.FirstOrDefault(u => u.Username == username);
+            if (user != null)
+            {
+                employeeDAL.DeleteEmployee(user.EmployeeID);
+                context.UserAccounts.Remove(user);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException("Tài khoản không tồn tại.");
+            }
+        }
+    }
+}
