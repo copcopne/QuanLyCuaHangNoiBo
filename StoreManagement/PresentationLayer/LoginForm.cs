4f903312d1be3facbee6b74842eefbcdb508be48
Si Nguyen <copcopne@gmail.com>
2025-08-12 20:16:55 +0700

    init tabControl dọc, update Authenticate

--- StoreManagement/PresentationLayer/LoginForm.cs
+++ StoreManagement/PresentationLayer/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,36 @@ namespace PresentationLayer
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Tên đăng nhập và mật khẩu là bắt buộc.");
+                return;
+            }
+            try
+            {
+                if (AuthenticateBUS.Authenticate(txtUsername.Text, txtPassword.Text))
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Lỗi");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ StoreManagement/PresentationLayer/VerticalTabControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public class VerticalTabControl : TabControl
+    {
+    }
+}
