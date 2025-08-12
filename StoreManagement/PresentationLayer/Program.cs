77ff1998bfa37c8f0b36725cdc1cdd9cc7f463dd
Thái Tuấn <ttuan.simple@gmail.com>
2025-08-12 13:28:41 +0700

    commit

--- StoreManagement/PresentationLayer/Program.cs
+++ StoreManagement/PresentationLayer/Program.cs
@@ -16,7 +16,7 @@ namespace PresentationLayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EmployeeManagementForm());
+            Application.Run(new InvoiceManagementForm());
         }
     }
 }
